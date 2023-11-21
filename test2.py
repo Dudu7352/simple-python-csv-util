927e5ba4fcec12648ed2d7eee40f9dfe40265818
Dudu7352 <aeiou7352@gmail.com>
2023-11-21 22:02:31 +0100

    Delete: remove unused files

--- test2.py
+++
@@ -1,7 +0,0 @@
-import re
-def to_snake(camel_case):
-    callback = lambda pat: f'{pat.group(1)[0]}_{pat.group(1)[1]}'
-    return re.sub(r'([a-z][A-Z])', callback, camel_case.replace(' ','_').replace('ID', 'id_')).lower()
-
-
-print(to_snake("IDucznia"))
--- test3.py
+++
@@ -1,9 +0,0 @@
-from inspect import signature
-
-
-class Elem:
-    def __init__(self, a: int, b: str, c: dict):
-        pass
-    
-
-print(signature(Elem).parameters['a'].annotation)
